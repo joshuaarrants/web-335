9fdc8d107918471bc2b4bbd1f5bf183d93cd9e32
Joshua Arrants <jwarrants@my.bellevue.edu>
2018-11-28 15:11:53 -0600

    adding arrants_calculator.py

---
+++ week-8/arrants_calculator.py
@@ -0,0 +1,12 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
